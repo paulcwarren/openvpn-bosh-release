e27f9becdcbcd6ca472ac856708ec37ca5b12bc7
Danny Berger <dpb587@gmail.com>
2017-06-11 04:35:03 -0700

    fix pipeline

--- ci/tasks/finalize-release/run.sh
+++ ci/tasks/finalize-release/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 task_dir=$PWD
 
@@ -38,32 +38,32 @@ EOF
 
 bosh finalize-release \
   --version="$version" \
-  "$task_dir/dev-release/$release_name-*.tgz"
+  "$task_dir/dev-release/$release_name"-*.tgz
 
 
 #
 # create the release tarball
 #
 
-echo "v$version" > $task_dir/master-release-artifacts/name
-git rev-parse HEAD > $task_dir/master-release-artifacts/commit
+echo "v$version" > $task_dir/release/name
+git rev-parse HEAD > $task_dir/release/commit
 
 if [ -e releases/*/*-$version.md ] ; then
-  cp releases/*/*-$version.md $task_dir/master-release-artifacts/notes.md
+  cp releases/*/*-$version.md $task_dir/release/notes.md
 else
-  touch $task_dir/master-release-artifacts/notes.md
+  touch $task_dir/release/notes.md
 fi
 
-tarball="$task_dir/master-release-artifacts/$release_name-$version.tgz"
+tarball="../release/$release_name-$version.tgz"
 
-bosh create-release --version="$version" --tarball="$tarball"
+bosh create-release --tarball="$tarball" "releases/$release_name/$release_name-$version.yml"
 
 metalink_path="releases/$release_name/$release_name-$version.meta4"
 
 meta4 create --metalink="$metalink_path"
 meta4 set-published --metalink="$metalink_path" "$( date -u +%Y-%m-%dT%H:%M:%SZ )"
 meta4 import-file --metalink="$metalink_path" --version="$version" "$tarball"
-meta4 file-set-url --metalink="$metalink_path" "https://$s3_bucket.s3.amazonaws.com/releases/$release_name-$version.tgz"
+meta4 file-set-url --metalink="$metalink_path" "https://$s3_bucket.s3.amazonaws.com/releases/$release_name/$release_name-$version.tgz"
 
 
 #
@@ -72,4 +72,4 @@ meta4 file-set-url --metalink="$metalink_path" "https://$s3_bucket.s3.amazonaws.
 
 git add -A .final_builds releases
 
-git commit -m "Finalize release (v$version)"
+git commit -m "Finalize release v$version"
