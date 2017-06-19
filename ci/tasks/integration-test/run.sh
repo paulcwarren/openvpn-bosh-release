0064b8e97ada4c1fd5e085194f8b8cdcb0d33088
Danny Berger <dpb587@gmail.com>
2017-06-18 21:47:31 -0700

    Split client behavior into openvpn-client job

--- ci/tasks/integration-test/run.sh
+++ ci/tasks/integration-test/run.sh
@@ -28,49 +28,49 @@ bosh -n deploy \
 
 bosh ssh role1/0 '
   set -e
-  sudo ping -c 5 192.168.206.1 | sudo tee -a /var/vcap/sys/log/openvpn/client1-stdout.log
+  sudo ping -c 5 192.168.206.1 | sudo tee -a /var/vcap/sys/log/openvpn-client/stdout.log
   sleep 5
-  sudo /var/vcap/bosh/bin/monit stop openvpn-client1
+  sudo /var/vcap/bosh/bin/monit stop openvpn-client
 '
 
 mkdir -p role1-logs
 
-bosh scp role1/0:/var/vcap/sys/log/openvpn/client1-stdout.log role1-logs
-bosh scp role1/0:/var/vcap/sys/log/openvpn/stdout.log role1-logs
+bosh scp role1/0:/var/vcap/sys/log/openvpn-client/stdout.log role1-logs/client-stdout.log
+bosh scp role1/0:/var/vcap/sys/log/openvpn/stdout.log role1-logs/stdout.log
 
-if ! grep -q "TCP connection established with" role1-logs/client1-stdout.log* ; then
+if ! grep -q "TCP connection established with" role1-logs/client-stdout.log* ; then
   fail "Client failed to connect to server"
-elif ! grep -q "/sbin/ifconfig tun1 192.168.206.2 netmask 255.255.255.0" role1-logs/client1-stdout.log* ; then
+elif ! grep -q "/sbin/ifconfig tun1 192.168.206.2 netmask 255.255.255.0" role1-logs/client-stdout.log* ; then
   fail "Client failed to establish tunnel correctly"
-elif ! grep -q "Initialization Sequence Completed" role1-logs/client1-stdout.log* ; then
+elif ! grep -q "Initialization Sequence Completed" role1-logs/client-stdout.log* ; then
   fail "Client did not complete initialization sequence"
-elif ! grep -q "64 bytes from 192.168.206.1" role1-logs/client1-stdout.log* ; then
+elif ! grep -q "64 bytes from 192.168.206.1" role1-logs/client-stdout.log* ; then
   fail "Client was unable to ping the remote gateway"
-elif ! grep -q "process exiting" role1-logs/client1-stdout.log* ; then
+elif ! grep -q "process exiting" role1-logs/client-stdout.log* ; then
   fail "Client did not exit cleanly"
 fi
 
 bosh ssh role2/0 '
   set -e
-  sudo ping -c 5 192.168.202.1 | sudo tee -a /var/vcap/sys/log/openvpn/client1-stdout.log
+  sudo ping -c 5 192.168.202.1 | sudo tee -a /var/vcap/sys/log/openvpn-client/stdout.log
   sleep 5
-  sudo /var/vcap/bosh/bin/monit stop openvpn-client1
+  sudo /var/vcap/bosh/bin/monit stop openvpn-client
 '
 
 mkdir -p role2-logs
 
-bosh scp role2/0:/var/vcap/sys/log/openvpn/client1-stdout.log role2-logs
-bosh scp role2/0:/var/vcap/sys/log/openvpn/stdout.log role2-logs
+bosh scp role2/0:/var/vcap/sys/log/openvpn-client/stdout.log role2-logs/client-stdout.log
+bosh scp role2/0:/var/vcap/sys/log/openvpn/stdout.log role2-logs/stdout.log
 
-if ! grep -q "TCP connection established with" role2-logs/client1-stdout.log* ; then
+if ! grep -q "TCP connection established with" role2-logs/client-stdout.log* ; then
   fail "Client failed to connect to server"
-elif ! grep -q "/sbin/ifconfig tun1 192.168.202.2 netmask 255.255.255.0" role2-logs/client1-stdout.log* ; then
+elif ! grep -q "/sbin/ifconfig tun1 192.168.202.2 netmask 255.255.255.0" role2-logs/client-stdout.log* ; then
   fail "Client failed to establish tunnel correctly"
-elif ! grep -q "Initialization Sequence Completed" role2-logs/client1-stdout.log* ; then
+elif ! grep -q "Initialization Sequence Completed" role2-logs/client-stdout.log* ; then
   fail "Client did not complete initialization sequence"
-elif ! grep -q "64 bytes from 192.168.202.1" role2-logs/client1-stdout.log* ; then
+elif ! grep -q "64 bytes from 192.168.202.1" role2-logs/client-stdout.log* ; then
   fail "Client was unable to ping the remote gateway"
-elif ! grep -q "process exiting" role2-logs/client1-stdout.log* ; then
+elif ! grep -q "process exiting" role2-logs/client-stdout.log* ; then
   fail "Client did not exit cleanly"
 fi
 
